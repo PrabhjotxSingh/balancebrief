81e729c8f29ce4c7f821d8550814caece4226d57
Kai Hoenshell <kaihoenshell@garythepmonster.wifi.uc.edu>
2023-11-04 21:53:58 -0400

    list comprehension, added to database

--- api/parse.py
+++ api/parse.py
@@ -2,9 +2,18 @@ import os
 import cohere
 import numpy as np
 
+from qdrant_client import QdrantClient
 from cohere.responses.classify import Example
+from qdrant_client.http.models import Distance, VectorParams
+from qdrant_client.http.models import PointStruct
 
-client = cohere.Client('lqJD3VjaVUpo4tgxxzg2RFIuu6p3jXmmezQc0tXD')
+client_Qdrant = QdrantClient(
+    url="https://e89b7298-c96f-4dbf-84ef-f0db87ed8f95.us-east4-0.gcp.cloud.qdrant.io:6333", 
+    api_key="O6zlMt_wp8A3UoefaS-lEoCWZE1Zm6WplaOwKxjMhB3KSSMyTe405w",
+)
+
+
+client_Cohere = cohere.Client('lqJD3VjaVUpo4tgxxzg2RFIuu6p3jXmmezQc0tXD')
 
 os.chdir("api/sources/") 
   
@@ -24,10 +33,19 @@ for file in os.listdir():
         temp = []
         for x in range(4):
             temp += [text_file.readline().replace("\n", '')]
-        meta_data.append(temp)
         text_file.readline()
-        text +=  [text_file.read().rstrip().replace("\n", ' ')]
+        body = [text_file.read().rstrip().replace("\n", ' ')]
+        text +=  body
+        temp += body
+        meta_data.append(temp)
 #Encode your documents with input type 'search_document'
+doc_emb = client_Cohere.embed(text, input_type="search_document", model="embed-english-v3.0").embeddings
 
-
-
+#Consolidate all captured data into a list of PointStructs to send to qDrant collection
+data = [PointStruct(id=i + 1, vector=doc_emb[i], payload = {"Bias": meta_data[i][0], "Author": meta_data[i][1], "Title": meta_data[i][2], "URL": meta_data[i][3], "Text": meta_data[i][4]}) for i in range(0, len(meta_data))]
+operation_info = client_Qdrant.upsert(
+    collection_name="Sources",
+    wait=True,
+    points= data,
+)
+print(data)
