951c73584d5b3cdcbb932a97031be14359d92c7a
Connor <mckinnr@mail.uc.edu>
2023-11-04 21:26:19 -0400

    Got parsing working

--- api/parse.py
+++ api/parse.py
@@ -1,11 +1,18 @@
-import os 
-path = "api/sources/"
-os.chdir(path) 
+import os
+import cohere
+import numpy as np
+
+from cohere.responses.classify import Example
+
+client = cohere.Client('lqJD3VjaVUpo4tgxxzg2RFIuu6p3jXmmezQc0tXD')
+
+os.chdir("api/sources/") 
   
 def read_text_file(file_path): 
     text_file = open(file_path, "r")
     return text_file
-  
+meta_data = []
+text = []
 # iterate through all file 
 for file in os.listdir(): 
     # Check whether file is in text format or not 
@@ -14,12 +21,13 @@ for file in os.listdir():
 
         # call read text file function
         text_file = read_text_file(file_path)
-        ret = []
-        for x in range(3):
-            temp = text_file.readline().replace("\n", '')
-            ret.append(temp)
+        temp = []
+        for x in range(4):
+            temp += [text_file.readline().replace("\n", '')]
+        meta_data.append(temp)
         text_file.readline()
-        print(ret)
-        text =  text_file.read().rstrip()
+        text +=  [text_file.read().rstrip().replace("\n", ' ')]
+#Encode your documents with input type 'search_document'
+
 
 
